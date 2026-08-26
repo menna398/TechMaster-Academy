66f0eeee93ec7cb4ca03162788a03538a93ad1e3
menna398 <mennakhaled398@gmail.com>
2026-08-26 21:45:03 +0300

    Add Sidebar Component

--- src/MainApplication/pages/Notes/Notes.jsx
+++ src/MainApplication/pages/Notes/Notes.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Notes() {
   return (
     <div>
-      
+      Hi from notes
     </div>
   )
 }
--- src/MainApplication/pages/Profile/Profile.jsx
+++ src/MainApplication/pages/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Profile() {
   return (
     <div>
-      
+      hi from profile
     </div>
   )
 }
